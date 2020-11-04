d9475e5cc7095636f96b8a4c3044b20bcc947d1a
leejalen <leejalen@foxmail.com>
2020-11-04 17:53:47 +0800

    1.Eureka的服务发现 （某个应用本身发现eureka服务器中所有的已注册的微服务，并获取他们的基本信息） 2.测试服务间的调用（RestTemplate）

--- provider-8001/src/main/java/com/example/provider8001/controller/ClassController.java
+++ provider-8001/src/main/java/com/example/provider8001/controller/ClassController.java
@@ -4,13 +4,19 @@ import com.example.provider8001.service.IClassService;
 import com.example.provider8001.service.bo.SpClassBO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created on 2020/10/31
- *
  * @author: leejalen
  */
 @Slf4j
@@ -21,9 +27,41 @@ public class ClassController {
     @Autowired
     private IClassService classService;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/get")
     public void getClassDto(){
         SpClassBO classBO = classService.selectClass("1");
         log.info(classBO.toString());
     }
+
+    /**
+     * 测试方法
+     * 发现eureka中所有服务的信息
+     * */
+    @GetMapping("/discovery")
+    public void discovery(){
+        //eureka中Application名列表
+        List<String> list = discoveryClient.getServices();
+        log.info("Application名列表 : {}", list);
+        //eureka中Status名列表
+        List<ServiceInstance> instanceList = discoveryClient.getInstances("PROVIDER");
+        for (ServiceInstance instance : instanceList){
+            log.info("Status名列表 : {}", instance);
+            String appId = instance.getServiceId();
+            String instanceId = instance.getInstanceId();
+            String host = instance.getHost();
+            int port = instance.getPort();
+            URI uri = instance.getUri();
+            Map<String, String> metaData = instance.getMetadata();
+            log.info("应用对外暴露的名字 : {}", appId);
+            log.info("应用在eureka中的别名 : {}", instanceId);
+            log.info("应用所在的主机IP : {}", host);
+            log.info("应用端口号 : {}", port);
+            log.info("应用真实IP地址 : {}", uri);
+            log.info("应用元数据 : {}", metaData);
+        }
+    }
+
 }
--- provider-8002/src/main/java/com/example/provider8002/controller/ClassController.java
+++ provider-8002/src/main/java/com/example/provider8002/controller/ClassController.java
@@ -7,11 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created on 2020/10/31
- *
- * @author: leejalen
+ * @author leejalen
  */
 @Slf4j
 @RestController
@@ -26,4 +26,16 @@ public class ClassController {
         SpClassBO classBO = classService.selectClass("1");
         log.info(classBO.toString());
     }
+
+    /**
+     * 测试服务间调用
+     * Provider8002使用RestTemplate通过IP地址调用Provider8001的服务
+     * */
+    @GetMapping("/testConnect")
+    public void testConnectProvider8001(){
+        RestTemplate restTemplate = new RestTemplate();
+        String host = "http://localhost:8001"+"/class/discovery";
+        restTemplate.getForObject(host, Object.class);
+    }
+
 }
