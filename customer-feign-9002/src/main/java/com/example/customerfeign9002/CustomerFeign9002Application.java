8570da63e232b32648a2836d99e5c696155bb7f4
leejalen <leejalen@foxmail.com>
2020-11-06 10:47:25 +0800

    新建子项目，添加Feign依赖

---
+++ customer-feign-9002/src/main/java/com/example/customerfeign9002/CustomerFeign9002Application.java
@@ -0,0 +1,13 @@
+package com.example.customerfeign9002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerFeign9002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerFeign9002Application.class, args);
+	}
+
+}
