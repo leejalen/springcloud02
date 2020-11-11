67cb0b09815fed2bc742cfaa0e495f52c3874e71
leejalen <leejalen@foxmail.com>
2020-11-11 17:17:32 +0800

    添加rabbitmq的springboot项目

--- rabbitmq-5001/src/main/java/com/example/rabbitmq5001/controller/RabbitProducerController.java
+++ rabbitmq-5001/src/main/java/com/example/rabbitmq5001/controller/RabbitProducerController.java
@@ -1,7 +1,33 @@
-package com.example.rabbitmq5001.controller;/**
+package com.example.rabbitmq5001.controller;
+
+import com.example.rabbitmq5001.service.IRabbitProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
  * @Description TODO
  * Created on 2020/11/11
  * @author leejalen
  */
+@Controller
+@RequestMapping("producer")
 public class RabbitProducerController {
+
+    @Autowired
+    private IRabbitProducerService rabbitProducerService;
+
+    @RequestMapping("/direct")
+    public void direct() {
+        for (int i = 0; i < 5; i++) {
+            this.rabbitProducerService.producerDirect("我是生产者，这是路由模式下的第" + i + "条信息");
+        }
+    }
+
+    @RequestMapping("/fanout")
+    public void fanout() {
+        for (int i = 0; i < 5; i++) {
+            this.rabbitProducerService.producerFanout("我是生产者，这是发布订阅模式下的第" + i + "条信息");
+        }
+    }
 }
--- rabbitmq-5001/src/main/java/com/example/rabbitmq5001/service/impl/RabbitConsumerServiceImpl.java
+++ rabbitmq-5001/src/main/java/com/example/rabbitmq5001/service/impl/RabbitConsumerServiceImpl.java
@@ -1,7 +1,51 @@
-package com.example.rabbitmq5001.service.impl;/**
+package com.example.rabbitmq5001.service.impl;
+
+import com.example.rabbitmq5001.service.IRabbitConsumerService;
+import com.example.rabbitmq5001.utils.Constants;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
  * @Description TODO
  * Created on 2020/11/11
  * @author leejalen
  */
-public class RabbitConsumerServiceImpl {
+@Component
+public class RabbitConsumerServiceImpl implements IRabbitConsumerService {
+
+    @Override
+    @RabbitListener(queues = Constants.QUEUE_DIRECT_A)
+    public void consumerDirectA(String message) {
+        System.out.println("消费者收到队列A中的消息:" + message);
+    }
+
+    @Override
+    @RabbitListener(queues = Constants.QUEUE_DIRECT_B)
+    public void consumerDirectB(String message) {
+        System.out.println("消费者收到队列B中的消息:" + message);
+    }
+
+    @Override
+    @RabbitListener(queues = Constants.QUEUE_FANOUT_C)
+    public void consumerFanoutC(String message) {
+        System.out.println("消费者收到队列C中的消息:" + message);
+    }
+
+    @Override
+    @RabbitListener(queues = Constants.QUEUE_FANOUT_D)
+    public void consumerFanoutD(String message) {
+        System.out.println("消费者收到队列D中的消息:" + message);
+    }
+
+    @Override
+    @RabbitListener(queues = Constants.QUEUE_TOPIC_E)
+    public void consumerTopicE(String message) {
+        System.out.println("消费者收到队列E中的消息:" + message);
+    }
+
+    @Override
+    @RabbitListener(queues = Constants.QUEUE_TOPIC_F)
+    public void consumerTopicF(String message) {
+        System.out.println("消费者收到队列F中的消息:" + message);
+    }
 }
